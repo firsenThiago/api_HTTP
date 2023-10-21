282c2d9e92a947e7845e6ff8cf2b10dcbdf30237
Thiago Firsen <thiagofirsen47@hotmail.com>
2023-10-21 01:53:48 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const routes = require("./routes");
+const { URL } = require("url");
+const bodyParser = require("./helpers/bodyParser");
+
+const server = http.createServer((request, response) => {
+  const parsedUrl = new URL(`http:localhost:3000${request.url}`);
+
+  let { pathname } = parsedUrl;
+  let id;
+
+  const splitEndPoint = pathname.split("/").filter(Boolean);
+
+  if (splitEndPoint.length > 1) {
+    pathname = `/${splitEndPoint[0]}/:id`;
+    id = splitEndPoint[1];
+  }
+
+  const route = routes.find(
+    (routeObject) =>
+      routeObject.endpoint === pathname && routeObject.method === request.method
+  );
+
+  if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { "Content-Type": "application/json" });
+      response.end(JSON.stringify(body));
+    };
+    if (["POST", "PUT", "PATCH"].includes(request.method)) {
+      bodyParser(request, () => route.handler(request, response));
+    } else {
+      route.handler(request, response);
+    }
+  } else {
+    response.writeHead(404, { "Content-Type": "text/html" });
+    response.end(`Cannot ${request.method} ${pathname}`);
+  }
+});
+
+server.listen(3000, () =>
+  console.log("Server started at http://localhost:3000")
+);
---
+++ src/mocks/users.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { id: 1, name: "Thiago" },
+  { id: 2, name: "Vitor" },
+  { id: 3, name: "Vinicius" },
+  { id: 4, name: "Igor" },
+  { id: 5, name: "Gabriel" },
+];
